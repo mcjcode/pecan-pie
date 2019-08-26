579daa47bb0e5c59dac3dc6ba4cea6284e7f855f
mcjcode <catalano_johnson@yahoo.com>
2019-08-26 09:31:45 -0400

    check whether base.raw file exists. strip racket output at and after 'context' line

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-fn=$1
+base=$1
+rawfn=$base.raw
 
-(echo "#lang pie"; mztext $fn.raw) > $fn.pie; racket $fn.pie
+if [ -e $rawfn ]; then
+    (echo "#lang pie"; mztext $rawfn) > $base.pie
+    # nothing after the 'context' line is useful to me.
+    racket $base.pie 2>&1 | awk 'BEGIN { x=1 } /context/ { x=0 } (x==1) {print $0}' 
+else
+    echo "$rawfn doesn't exist"
+fi
