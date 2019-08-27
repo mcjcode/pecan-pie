ee35e6bc8170d93d59c75c49d34197b3e51756dc
mcjcode <catalano_johnson@yahoo.com>
2019-08-27 12:50:24 -0400

    improve stripping context lines via more specific regexp

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ rawfn=$base.raw
 if [ -e $rawfn ]; then
     (echo "#lang pie"; mztext $rawfn) > $base.pie
     # nothing after the 'context' line is useful to me.
-    racket $base.pie 2>&1 | awk 'BEGIN { x=1 } /context/ { x=0 } (x==1) {print $0}' 
+    racket $base.pie 2>&1 | awk 'BEGIN { x=1 } /context\.\.\./ { x=0 } (x==1) {print $0}' 
 else
     echo "$rawfn doesn't exist"
 fi
