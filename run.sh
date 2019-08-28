f6a96e7e085193e33a60453141773f8ac32fc933
mcjcode <catalano_johnson@yahoo.com>
2019-08-28 15:01:49 -0400

    trying to get racket exit codes passed through correctly.  trying

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ rawfn=$base.raw
 if [ -e $rawfn ]; then
     (echo "#lang pie"; mztext $rawfn) > $base.pie
     # nothing after the 'context' line is useful to me.
+    echo "running $base.pie"
     racket $base.pie 2>&1 | awk 'BEGIN { x=1 } /context\.\.\./ { x=0 } (x==1) {print $0}' 
 else
     echo "$rawfn doesn't exist"
+    exit 1
 fi
