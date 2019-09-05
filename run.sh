362495b421625f224c105fcb017331beef63f6de
mcjcode <catalano_johnson@yahoo.com>
2019-09-04 20:11:48 -0400

    added tee to record racket output, just in case we care

--- run.sh
+++ run.sh
@@ -6,8 +6,10 @@ rawfn=$base.raw
 if [ -e $rawfn ]; then
     (echo "#lang pie"; mztext $rawfn) > $base.pie
     # nothing after the 'context' line is useful to me.
+    #
+    # copy the output to out.pie 
     echo "running $base.pie"
-    racket $base.pie 2>&1 | awk 'BEGIN { x=1 } /context\.\.\./ { x=0 } (x==1) {print $0}' 
+    racket $base.pie 2>&1 | awk 'BEGIN { x=1 } /context\.\.\./ { x=0 } (x==1) {print $0}' | tee out.pie
 else
     echo "$rawfn doesn't exist"
     exit 1
